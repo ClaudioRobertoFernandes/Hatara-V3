41f987459743de1d0cc6df21e77f992129b25771
ClaudioRobertoFernandes <claudio.fernandes@avantis.edu.br>
2023-09-28 16:14:47 -0300

    Send totals files from this project

---
+++ resources/views/livewire/result.blade.php
@@ -0,0 +1,15 @@
+@section('title', 'Result')
+<div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Result') }}
+        </h2>
+    </x-slot>
+    <div class="py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                ola hatara tudo bem vamos corrigir daqui para frente
+            </div>
+        </div>
+    </div>
+</div>
