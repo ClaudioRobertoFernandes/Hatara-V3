6e855942bd5d0e194fa54b82cd082469ef9bd1fd
ClaudioRobertoFernandes <claudio.fernandes@avantis.edu.br>
2023-10-20 17:14:04 -0300

    Adjusts for commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
     <body>
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
-
                 <div class="mt-5">
                     <div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 px-4">
                         <a href="{{ route('dashboard') }}" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
@@ -68,7 +67,6 @@
                         </a>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
     </body>
