bf94c0b92fc0dc8ee8576e5af65223db0ff4697e
ClaudioRobertoFernandes <claudio.fernandes@avantis.edu.br>
2023-10-25 20:30:27 -0300

    Submit a first version from new register user

--- app/Livewire/Clients/ClientsComponent.php
+++ app/Livewire/Clients/ClientsComponent.php
@@ -2,9 +2,14 @@
 
 namespace App\Livewire\Clients;
 
+use App\Enums\UserTypes\UserTypes;
 use App\Models\Users\User;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
@@ -23,6 +28,38 @@ class ClientsComponent extends Component implements HasForms, HasTable
     {
         return $table
             ->query(User::query()->with(['address', 'dataUser']))
+            ->headerActions([
+                CreateAction::make()
+                    ->label('Novo cliente')
+                    ->modalWidth('sm')
+                    ->modalHeading('Novo cliente')
+                    ->model(User::class)
+                    ->form([
+                        TextInput::make('name')
+                            ->label('Nome')
+                            ->string()
+                            ->minLength(3)
+                            ->maxLength(255)
+                            ->required()
+                            ->placeholder('Nome do cliente')
+                            ->live(),
+                        TextInput::make('email')
+                            ->label('E-mail')
+                            ->email()
+                            ->unique(table: User::class)
+                            ->required()
+                            ->placeholder('E-mail do cliente')
+                            ->live(),
+                        Select::make('user_type_id')
+                            ->label('Tipo de usuário')
+                            ->options(UserTypes::class)
+                            ->required()
+                            ->placeholder('Selecione uma opção')
+                            ->live(),
+                    ])
+                    ->createAnother(false)
+                    ->successRedirectUrl(fn(User $record): string => route('clients-edit', ['user' => $record]))
+            ])
             ->columns([
                 TextColumn::make('name')
                     ->label('Nome')
@@ -48,7 +85,16 @@ class ClientsComponent extends Component implements HasForms, HasTable
                     ->label('Estado')
                     ->sortable()
                     ->searchable(),
-            ]);
+            ])
+            ->striped()
+            ->actions([
+                    EditAction::make('edit')
+                        ->successRedirectUrl(fn(User $record): string => route('clients-edit', [
+                            'user' => $record,
+                        ]))
+                        ->successNotification(null)
+                ]
+            );
     }
 
     public function render(): View|Factory|Application
